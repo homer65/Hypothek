041dbf0c1d4393552686ef2707ae54246f221944
Christian Ehm <christian@myoggradio.org>
2020-04-17 11:10:58 +0200

    Summe der Zahlungen wird ausgegeben

--- Hypothek/src/pack/Verarbeitung.java
+++ Hypothek/src/pack/Verarbeitung.java
@@ -8,6 +8,7 @@ public class Verarbeitung
 		double monatliche_zahlung = Parameter.monatliche_zahlung;
 		double zins_satz = Parameter.zins_satz;
 		double jaehrliche_sonderzahlung = Parameter.jaehrliche_sonderzahlung;
+		double bisher_gezahlt = 0.0;
 		boolean abbruch = false;
 		int lfdnr = 0;
 		em.addSatz("<<<< " + f(zins_satz) + " " +f(monatliche_zahlung) + " " + f(rest_kapital) );
@@ -16,13 +17,29 @@ public class Verarbeitung
 			double zins = rest_kapital * zins_satz / 1200.0;
 			double tilgung = monatliche_zahlung - zins;
 			rest_kapital = rest_kapital - tilgung;
-			if (rest_kapital <= 0.0) abbruch = true;
-			em.addSatz(f(lfdnr) + " " + f(monatliche_zahlung) + " " + f(zins) + " " + f(tilgung) + " " + f(rest_kapital));
+			bisher_gezahlt += monatliche_zahlung;
+			if (rest_kapital <= 0.0)
+			{
+				abbruch = true;
+				monatliche_zahlung += rest_kapital;
+				tilgung += rest_kapital;
+				bisher_gezahlt += rest_kapital;
+				rest_kapital = 0.0;
+			}
+			em.addSatz(f(lfdnr) + " " + f(monatliche_zahlung) + " " + f(zins) + " " + f(tilgung) + " " + f(rest_kapital) + " " + f(bisher_gezahlt));
 			double monat = lfdnr - ((lfdnr / 12) * 12) +1;
 			if (monat == 12) 
 			{
 				rest_kapital = rest_kapital - jaehrliche_sonderzahlung;
-				em.addSatz(">>>> " + f(jaehrliche_sonderzahlung) + " " + f(rest_kapital));
+				bisher_gezahlt += jaehrliche_sonderzahlung;
+				if (rest_kapital <= 0.0)
+				{
+					abbruch = true;
+					jaehrliche_sonderzahlung += rest_kapital;
+					bisher_gezahlt += rest_kapital;
+					rest_kapital = 0.0;
+				}
+				em.addSatz(">>>> " + f(jaehrliche_sonderzahlung) + " " + f(rest_kapital) + " " + f(bisher_gezahlt));
 			}
 			lfdnr++;
 		}
